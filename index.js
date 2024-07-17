ad3ddb263f1bd5954a7d15c0efb642f79517ee6f
VestedNoman <noman@lendvest.io>
2024-07-17 17:12:52 +0500

    class-01

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+
+var box = document.getElementById('box');
+
+
+
+
+function functionCall(){
+    box.style.backgroundColor = 'green';
+    box.style.borderRadius = '10%';
+    box.style.position = 'absolute';
+    box.style.left = '40%'
+    box.style.border = '10px solid red'
+}
